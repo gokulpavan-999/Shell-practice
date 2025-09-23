1020d98c5b1f13ee0040241f51697347b174bca8
gokulpavan-999 <pavanthota480@gmail.com>
2025-09-23 14:35:25 +0530

    Create 03-variables.sh

---
+++ 03-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+PERSON1=Gokul
+PERSON2=Pavan
+
+echo "$PERSON1:: Hey $PERSON2, have you heard about DevOps?"
+echo "$PERSON2:: I’ve heard the word, but I don’t really know what it means."
+echo "$PERSON1:: No worries. DevOps is all about combining Development and Operations to deliver software faster and more reliably."
+echo "$PERSON2:: Oh, so it’s not just coding?"
+echo "$PERSON1:: Exactly. It’s also about automation, testing, deployment, monitoring, and teamwork."
+echo "$PERSON2:: Interesting! Where should I start if I want to learn?"
