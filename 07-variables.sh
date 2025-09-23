8ff12b5c3319c2c0a54dbe871779860c8c898f8d
gokulpavan-999 <pavanthota480@gmail.com>
2025-09-23 18:22:35 +0530

    Update 07-variables.sh

--- 07-variables.sh
+++ 07-variables.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-DATE=$(date)
-echo "Time Stamp Executed : $DATE"
+#DATE=$(date)
+#echo "Time Stamp Executed : $DATE"
+
+START_TIME=$(date +%s)
+
+sleep 10
+
+END_TIME=$(date +%s)
+
+TOTAL_TIME=$(($END_TIME-$START_TIME))
+echo "Script exected in : $TOTAL_TIME Seconds"
