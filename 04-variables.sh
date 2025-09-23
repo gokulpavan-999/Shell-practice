3b458615dad783a5e7e4920489c798df518ea348
gokulpavan-999 <pavanthota480@gmail.com>
2025-09-23 15:13:28 +0530

    Update 04-variables.sh

--- 04-variables.sh
+++ 04-variables.sh
@@ -5,7 +5,7 @@ $PERSON2=$2
 
 echo "$PERSON1:: Hi $PERSON2, how are you?"
 echo "$PERSON2:: I’m good, $PERSON1. How about you?"
-echo "$PERSON1: I’m fine too. Did you finish your homework?"
-echo "$PERSON2: Yes, I finished it yesterday."
-echo "$PERSON1: Great! Let’s play cricket now."
-echo "$PERSON2: Sure, let’s go!"
+echo "$PERSON1:: I’m fine too. Did you finish your homework?"
+echo "$PERSON2:: Yes, I finished it yesterday."
+echo "$PERSON1:: Great! Let’s play cricket now."
+echo "$PERSON2:: Sure, let’s go!"
