67ee2d022b8a946475c200fd01687ecb22a2bdb3
gokulpavan-999 <pavanthota480@gmail.com>
2025-09-23 14:21:52 +0530

    Create 02-conversation.sh

---
+++ 02-conversation.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Ramesh:: Hey Suresh, have you heard about DevOps?"
+echo "Suresh:: I’ve heard the word, but I don’t really know what it means."
+echo "Ramesh:: No worries. DevOps is all about combining Development and Operations to deliver software faster and more reliably."
+echo "Suresh:: Oh, so it’s not just coding?"
+echo "Ramesh:: Exactly. It’s also about automation, testing, deployment, monitoring, and teamwork."
+echo "Suresh:: Interesting! Where should I start if I want to learn?"
