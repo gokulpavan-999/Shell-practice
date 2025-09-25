7d79bef59946d30cd3b809109994645996cdb0dd
gokulpavan-999 <pavanthota480@gmail.com>
2025-09-25 16:01:57 +0530

    Create 16-logs.sh

---
+++ 16-logs.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/Shell-script"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" #/var/log/Shell-script/16.logs.log
+
+mkdir -p $LOGS_FOLDER
+echo "scrip started executed at :$(date)"
+
+if [ $USERID -ne 0 ]; then
+   echo "ERROR:: Please run this script with root privilege"
+   exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+       echo -e "ERROR:: Installing $2 ... $R FAILURE $N"
+    else
+       echo -e "Installing $2 ... $G SUCCESS $N"
+    fi
+}
+
+dnf list installed mysql
+    if [ $? -ne 0 ]; then
+       dnf install mysql -y
+       VALIDATE $? "MySQL"
+    else
+      echo -e "MySQL already exist ... $Y SKIPPING $N"
+    fi
+
+dnf list installed nginx
+    if [ $? -ne 0 ]; then
+       dnf install nginx -y
+       VALIDATE $? "Nginx"
+    else
+      echo -e "Nginx already exist ... $Y SKIPPING $N"
+    fi
+
+dnf list installed python3
+    if [ $? -ne 0 ]; then
+       dnf install python3 -y
+       VALIDATE $? " Python3"
+    else
+      echo -e "Python3 already exists ... $Y SKIPPING $N"
+    fi
