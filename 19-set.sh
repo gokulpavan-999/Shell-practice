e8f112c6ba21ffa1f20fe658978f320ceeb4d7fe
gokulpavan-999 <pavanthota480@gmail.com>
2025-09-29 16:21:30 +0530

    Update 19-set.sh

--- 19-set.sh
+++ 19-set.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-trap 'echo "There is an error in $LINENO, Command is: $BASH_COMMAND"'ERR
+trap 'echo "There is an error in $LINENO, Command is: $BASH_COMMAND"' ERR
 
 echo "Hello.."
 echo "Before error"
