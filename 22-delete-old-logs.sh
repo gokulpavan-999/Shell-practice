c4378e0c84c80aea51a93549c7a87226249526ba
gokulpavan-999 <pavanthota480@gmail.com>
2025-10-03 12:56:46 +0530

    Create 22-delete-old-logs.sh

---
+++ 22-delete-old-logs.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/Shell-roboshop"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+mkdir -p $LOGS_FOLDE
+echo "Script Staring at: $(date)"
+
+SOURCE_DIR="/home/ec2-user/app-logs"
+
+if [ ! -d $SOURCE_DIR ]; then
+     echo -e "ERROR:: $SOURCE_DIR does not exist"
+     exit 
+fi
+
+FILES_TO_DELETE=$( find $SOURCE_DIR -name "*.log" )
+
+while IFS= read -r filepath 
+do
+  echo "Deleting the file: $filepath"
+  
+done <<<$FILE_TO_DELRTE
