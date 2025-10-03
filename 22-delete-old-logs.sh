2f98c4fbbee4f1078ae35c1f7b3c53055a62c306
gokulpavan-999 <pavanthota480@gmail.com>
2025-10-03 15:00:34 +0530

    Update 22-delete-old-logs.sh

--- 22-delete-old-logs.sh
+++ 22-delete-old-logs.sh
@@ -13,17 +13,17 @@ LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 mkdir -p $LOGS_FOLDER
 echo "Script Staring at: $(date)"
 
-SOURCE_DIR=/home/ec2-user/app-logs
+SOURCE_DIR="/home/ec2-user/app-logs"
 
 if [ ! -d $SOURCE_DIR ]; then
      echo -e "ERROR:: $SOURCE_DIR does not exist"
      exit 
 fi
 
-FILES_TO_DELETE=$(find $SOURCE_DIR -name "*.log")
+FILES_TO_DELETE=$(find $SOURCE_DIR -name "*.log" -mtime +14)
 
 while IFS= read -r filepath 
 do
   echo "Deleting the file: $filepath"
   
-done <<< $FILE_TO_DELRTE
+done <<<"$FILE_TO_DELRTE"
