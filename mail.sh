d005484f01487418d8b5a1fa9fd472427f1112fa
gokulpavan-999 <pavanthota480@gmail.com>
2025-10-06 15:04:02 +0530

    Update mail.sh

--- mail.sh
+++ mail.sh
@@ -8,12 +8,12 @@ FORMATTED_BODY=$(printf '%s\n' "MESSAGE_BODY" | sed -e 's/[]\/$*.^[]/\\&/g')
 IP_ADDRESS=$5
 TO_TEAM=$6
 
-EMAIL_BODY=$(sed -e "s/TO_TEAM/$TO_TEAM/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/IP_ADDRESS/$IP_ADDRESS/g" -e "s/MESSAGE/$EMAIL_BODY/g" template.html)
+FINAL_BODY=$(sed -e "s/TO_TEAM/$TO_TEAM/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/IP_ADDRESS/$IP_ADDRESS/g" -e "s/MESSAGE/$FORMATTED_BODY/g" template.html)
 
 {
 echo "To: $TO_ADDRESS"
 echo "Subject: $SUBJECT"
 echo "Content-Type: text/html"
 echo ""
-echo "$EMAIL_BODY"
+echo "$FINAL_BODY"
 } | msmtp "$TO_ADDRESS"
